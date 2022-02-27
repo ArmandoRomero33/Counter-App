dbe625c667000259fbabebab2c1360812455183c
Armando <armamdoromeroorbe33@gmail.com>
2022-02-27 17:29:08 -0600

    Eventos click (Eventos en General)

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -2,10 +2,16 @@ import React from 'react'
 import ProTypes from 'prop/types'
 
 const CounterApp = ({ value }) => {
+    
+    const handleadd = (e) => {
+        console.log(e)
+    }
     return (
         <>
              <h1>CounterApp</h1>
              <h2> { value } </h2>
+
+             <button onClick={ (e) { console.log(e) } }lick>+1</button>
 
         </>
     )
